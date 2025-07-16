44a08be7f45036fbe9c07a5e662019eb37cfc2cd
raul-tb <raultamayobriones@gmail.com>
2025-07-15 23:11:48 -0600

    add cards styles

--- src/features/pokedexSlice.js
+++ src/features/pokedexSlice.js
@@ -3,7 +3,22 @@ import axios from 'axios';
 
 export const fetchPokemons = createAsyncThunk('pokedex/fetchPokemons', async () => {
     const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=20');
-    return response.data.results;
+    const pokemonBasicList = response.data.results;
+
+    const detailedList = await Promise.all(
+        pokemonBasicList.map(async (pokemon) => {
+            const details = await axios.get(pokemon.url);
+            return {
+                name: pokemon.name,
+                id: details.data.id,
+                image: details.data.sprites.front_default,
+                types: details.data.types.map(t => t.type.name),
+            };
+
+        })
+    );
+
+    return detailedList;
 });
 
 const pokedexSlice = createSlice({
--- src/features/pokemonList/PokemonList.jsx
+++ src/features/pokemonList/PokemonList.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchPokemons } from '../pokedexSlice';
-import { Link } from 'react-router-dom';
-import './PokemonList.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPokemons } from "../pokedexSlice";
+import { Link } from "react-router-dom";
+import "./PokemonList.css";
 
 const PokemonList = () => {
   const dispatch = useDispatch();
@@ -10,20 +10,50 @@ const PokemonList = () => {
   const status = useSelector((state) => state.pokedex.status);
 
   useEffect(() => {
-    if (status === 'idle') {
+    if (status === "idle") {
       dispatch(fetchPokemons());
     }
   }, [status, dispatch]);
 
-  if (status === 'loading') return <p>Cargando Pokédex...</p>;
-  if (status === 'failed') return <p>Error al cargar Pokédex.</p>;
+  if (status === "loading") return <p>Cargando Pokédex...</p>;
+  if (status === "failed") return <p>Error al cargar Pokédex.</p>;
+
+  function getColorByType(type) {
+    switch (type) {
+      case "grass":
+        return "#78C850";
+      case "fire":
+        return "#F08030";
+      case "water":
+        return "#6890F0";
+      case "electric":
+        return "#F8D030";
+      case "psychic":
+        return "#F85888";
+      case "bug":
+        return "#A8B820";
+      case "normal":
+        return "#A8A878";
+      default:
+        return "#f4f4f4";
+    }
+  }
 
   return (
     <div className="pokemon-grid">
-      {pokemons.map((pokemon, index) => (
-        <Link key={index} to={`/detail/${pokemon.name}`} className="pokemon-card">
-          <h3>{pokemon.name.toUpperCase()}</h3>
-          <p>ID: {index + 1}</p>
+      {pokemons.map((pokemon) => (
+        <Link
+          key={pokemon.id}
+          to={`/detail/${pokemon.name}`}
+          className="pokemon-card"
+          style={{ backgroundColor: getColorByType(pokemon.types[0]) }}
+        >
+          <div className="pokemon-info">
+            <h3>{pokemon.name.toUpperCase()}</h3>
+            <p>Tipo: {pokemon.types[0]}</p>
+            <p>ID: {pokemon.id}</p>
+          </div>
+          <img src={pokemon.image} alt={pokemon.name} />
         </Link>
       ))}
     </div>
